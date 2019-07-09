4380955eeb1015c817858fb4b11165ab2e3146f9
Laurent Noudohounsi <laurent.noudohounsi@mybraintech.com>
2019-07-09 14:56:33 +0200

    Handle envelope inside NetworkOperation

--- Network/Session/GetSessionsHistory.swift
+++ Network/Session/GetSessionsHistory.swift
@@ -43,7 +43,7 @@ final class GetSessionsHistoryRequest: RequestProtocol {
 }
 
 final class GetSessionsHistoryOperation: NetworkOperation<
-MeloRequestModel<SessionHistory>,
+SessionHistory,
 GetSessionsHistoryRequest
 > {
   init(dependecies: [Operation]? = nil) {
--- Network/ViewController.swift
+++ Network/ViewController.swift
@@ -17,24 +17,23 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    
     getSessions() { result in
 
       // We can use the result in (at least) 3 different ways
-
       // 1) with switch
-      switch result {
-      case .success(let history): print("succes: \(history)")
-      case .failure(let error): print("error : \(error)")
-      }
-
-      // 2) with do-catch
-      do {
-        let history = try result.get()
-        print("succes: \(history)")
-      } catch {
-        print("error : \(error)")
-      }
+      //switch result {
+      //case .success(let history): print("succes: \(history)")
+      //case .failure(let error): print("error : \(error)")
+      //}
+      //
+      //// 2) with do-catch
+      //do {
+      //  let history = try result.get()
+      //  print("succes: \(history)")
+      //} catch {
+      //  print("error : \(error)")
+      //}
 
       // 3) or the beloved guard
       guard let history = try? result.get() else {
@@ -58,17 +57,7 @@ class ViewController: UIViewController {
   ///   - failure: failure closure with error message
   func getConfig(completion: ((Result<Config, Error>) -> Void)?) {
     let operation = GetConfigOperation()
-
-//    operation.completionBlock = { completion?(operation.result) }
-
-    // Or
-
-    // Force wrapping in `result.data.first!` is temporary. The model will
-    // return the good type `result.data.first` directly soon.
-    // Stay tunned 🤓
-    operation.success = { result in completion?(.success(result.data.first!))}
-    operation.failure = { error in completion?(.failure(error)) }
-
+    operation.completionBlock = { completion?(operation.result) }
     NetworkQueue.shared.addOperation(operation: operation)
   }
 
@@ -79,19 +68,14 @@ class ViewController: UIViewController {
   ///   - failure: failure closure with error message
   func getSessions(completion: ((Result<Session, Error>) -> Void)?) {
     let operation = GetSessionsOperation()
-
-    operation.success = { result in completion?(.success(result.data.first!))}
-    operation.failure = { error in completion?(.failure(error)) }
-
+    operation.completionBlock = { completion?(operation.result) }
     NetworkQueue.shared.addOperation(operation: operation)
   }
 
   typealias SessionHistoryResult = Result<SessionHistory, Error>
   func getSessionsHistory(completion: ((SessionHistoryResult) -> Void)?) {
     let operation = GetSessionsHistoryOperation()
-    operation.success = { result in completion?(.success(result.data.first!))}
-    operation.failure = { error in completion?(.failure(error)) }
-
+    operation.completionBlock = { completion?(operation.result) }
     NetworkQueue.shared.addOperation(operation: operation)
   }
 
